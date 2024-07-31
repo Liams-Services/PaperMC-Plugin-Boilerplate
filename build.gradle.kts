35d3b6cbd4208a9aa6cc576220207642cfc2d9a9
CoasterFreakDE <mleaked@gmail.com>
2024-07-31 18:51:50 +0200

    feat: 1.21 Support

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
     kotlin("plugin.serialization") version "2.0.0"
 }
 
+val pluginVersion: String by project
+
 group = "com.liamxsage.boilerplates"
-version = "2.0-SNAPSHOT"
+version = pluginVersion
 
 val minecraftVersion: String by project
 val slf4jVersion: String by project
@@ -22,20 +24,34 @@ val kotlinxCollectionsImmutableVersion: String by project
 
 val gsonVersion: String by project
 
+val ktorVersion: String by project
+
+val mcCoroutineVersion: String by project
+
 repositories {
     maven("https://nexus.flawcra.cc/repository/maven-mirrors/")
 }
 
 val deliverDependencies = listOf(
+    "com.google.code.findbugs:jsr305:3.0.2",
+    "com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:$mcCoroutineVersion",
+    "com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:$mcCoroutineVersion",
+
     "org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinxCoroutinesCoreVersion",
     "org.jetbrains.kotlinx:kotlinx-collections-immutable:$kotlinxCollectionsImmutableVersion",
     "com.google.code.gson:gson:$gsonVersion",
+
+    "io.ktor:ktor-client-core-jvm:$ktorVersion",
+    "io.ktor:ktor-client-cio-jvm:$ktorVersion",
+    "io.ktor:ktor-client-json-jvm:$ktorVersion",
+    "io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion",
+    "io.ktor:ktor-client-serialization-jvm:$ktorVersion",
+    "io.ktor:ktor-client-content-negotiation-jvm:$ktorVersion",
 
     "dev.fruxz:ascend:$fruxzAscendVersion",
     "dev.fruxz:stacked:$fruxzStackedVersion",
 
     "io.github.cdimascio:dotenv-kotlin:$dotenvKotlinVersion", // - .env support
-
 
     "org.jetbrains.exposed:exposed-core:$exposedVersion",
     "org.jetbrains.exposed:exposed-dao:$exposedVersion",
@@ -50,7 +66,7 @@ val includedDependencies = mutableListOf<String>()
 
 fun Dependency?.deliver() = this?.apply {
     val computedVersion = version ?: kotlin.coreLibrariesVersion
-    includedDependencies += "${group}:${name}:${computedVersion}"
+    includedDependencies.add("${group}:${name}:${computedVersion}")
 }
 
 dependencies {
@@ -64,16 +80,34 @@ dependencies {
     }
 }
 
+tasks.register("generateDependenciesFile") {
+    group = "build"
+    description = "Writes dependencies to file"
+
+    val dependenciesFile = File(layout.buildDirectory.asFile.get(), "generated-resources/.dependencies")
+    outputs.file(dependenciesFile)
+    doLast {
+        dependenciesFile.parentFile.mkdirs()
+        dependenciesFile.writeText(includedDependencies.joinToString("\n"))
+    }
+}
+
+
 tasks {
     build {
         dependsOn(reobfJar)
     }
 
     withType<ProcessResources> {
+        dependsOn("generateDependenciesFile")
+
+        from(File(layout.buildDirectory.asFile.get(), "generated-resources")) {
+            include(".dependencies")
+        }
+
         expand(
             "version" to project.version,
             "name" to project.name,
-            "dependencies" to includedDependencies.joinToString("\n")
         )
     }
 
